fedc0f4ad7f5ab2b52c2d866d26b8785869d72b3
Fabiana Santos <fabianasantosef@gmail.com>
2021-04-24 13:29:25 -0300

    criando o objeto gson

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,25 +1,29 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import com.google.gson.reflect.TypeToken;
 
-import model.Elevador;
+import src.main.java.model.UsoElevador;
 
 public class Main {
-	 public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-	        String json
-	                = String.join(" ",
-	                        Files.readAllLines(
-	                                Paths.get("./input.json"),
-	                                StandardCharsets.UTF_8)
-	                );
+		// --- convertendo para json --- //
+		Gson gson = new Gson();
+		BufferedReader br = new BufferedReader(new FileReader("input.json"));
+		UsoElevador[] cliente = gson.fromJson(br, UsoElevador[].class);
 
-	        Elevador elevador = new Gson().fromJson(json, Elevador.class);
-	    }
+		// --- exibindo json --- //
+		for (UsoElevador uso : lista) {
+			System.out.println(uso);
+		}
 
+	}
 }
---
+++ src/main/java/model/UsoElevador.java
@@ -0,0 +1,31 @@
+package src.main.java.model;
+
+public class UsoElevador {
+	private Integer andar;
+	private Character elevador;
+	private Character turno;
+	
+	public Integer getAndar() {
+		return andar;
+	}
+	public void setAndar(Integer andar) {
+		this.andar = andar;
+	}
+	public Character getElevador() {
+		return elevador;
+	}
+	public void setElevador(Character elevador) {
+		this.elevador = elevador;
+	}
+	public Character getTurno() {
+		return turno;
+	}
+	public void setTurno(Character turno) {
+		this.turno = turno;
+	}
+	
+	
+	
+	
+
+}
